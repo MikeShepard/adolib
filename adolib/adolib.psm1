75303730e2333097d538705c24adb19b5e3cbc6e
Mike Shepard <mshepard@jackhenry.com>
2020-09-29 13:42:55 -0500

    Removed StrictMode !deploy

--- adolib/adolib.psm1
+++ adolib/adolib.psm1
@@ -1,6 +1,4 @@
 
-Set-StrictMode -Version Latest
-
 #Get public and private function definition files.
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
